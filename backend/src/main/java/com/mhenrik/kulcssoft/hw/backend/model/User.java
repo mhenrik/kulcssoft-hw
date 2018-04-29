c18bc5b7e9e3a72f17683fd71e69cce4492c7dd5
Henrik Miszoglad <henrik.miszoglad@gmail.com>
2018-04-29 07:52:10 +0200

    user added

---
+++ backend/src/main/java/com/mhenrik/kulcssoft/hw/backend/model/User.java
@@ -0,0 +1,56 @@
+package com.mhenrik.kulcssoft.hw.backend.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "usertable")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    public User() {
+    }
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public User(long id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
